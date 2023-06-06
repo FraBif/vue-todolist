0e0529464c6d2c144ed4525b31b421e5941c5246
FraBif <francescobif@hotmail.it>
2023-06-06 17:21:25 +0200

    not visualizing in page

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 const { createApp } = Vue
 
 createApp({
-
-data() {
-    return {
-        toDoList: [
-            {
-                text: 'Provare a capire qualcosa della programmazione',
-                done: false,
-            
-            }, {
-                text: 'Studiare la teoria della programmazione',
-                done: false,
+    data(){
+        return {
+            toDoList : [
+                {
+                    text: 'Provare a capire qualcosa della programmazione',
+                    done: false,
                 
-            }, {
-                text: 'Provare ad applicare la teoria alla pratica',
-                done: false,
-            }, {
-                text: 'Rendersi conto che qualcosa non va',
-                done: true,
-            }, {
-                text: 'Piangere sul divanetto del Napoli',
-                done: true,
-            }
-        ] ,
+                }, {
+                    text: 'Studiare la teoria della programmazione',
+                    done: false,
+                    
+                }, {
+                    text: 'Provare ad applicare la teoria alla pratica',
+                    done: false,
+                }, {
+                    text: 'Rendersi conto che qualcosa non va',
+                    done: true,
+                }, {
+                    text: 'Piangere sul divanetto del Napoli',
+                    done: true,
+                }
+            ] ,
+            newToDo : ''
+        }
 
-        newToDo : '' ,
-
-        methods : 
-     }
-        
-        
-
-
-}
-
-
+    
+    },
+    methods : 
 })
+
+
+
+
