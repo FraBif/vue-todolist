9ea0dc7287cf74be8d1dddd689979f09c0b05ffe
FraBif <francescobif@hotmail.it>
2023-06-06 16:39:08 +0200

    updated objects array

---
+++ script.js
@@ -0,0 +1,39 @@
+const { createApp } = Vue
+
+createApp({
+
+data() {
+    return {
+        toDoList: [
+            {
+                text: 'Provare a capire qualcosa della programmazione',
+                done: false,
+            
+            }, {
+                text: 'Studiare la teoria della programmazione',
+                done: false,
+                
+            }, {
+                text: 'Provare ad applicare la teoria alla pratica',
+                done: false,
+            }, {
+                text: 'Rendersi conto che qualcosa non va',
+                done: true,
+            }, {
+                text: 'Piangere sul divanetto del Napoli',
+                done: true,
+            }
+        ] ,
+
+        newToDo : '' ,
+
+        methods : 
+     }
+        
+        
+
+
+}
+
+
+})
