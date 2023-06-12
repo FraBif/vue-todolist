0be10c6d7dd83870eefaabe3fcffa939977c36e7
FraBif <francescobif@hotmail.it>
2023-06-12 17:50:31 +0200

    Rewrote script and main now it works

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-const {createApp} = Vue
+const {createApp} = Vue;
 
-createApp ({
-    data () {
+createApp({
+    data(){
         return{
-            toDoList: [{
-                text: "Capire qualcosa della programmazione",
-                done: false
-            },
-    
-                {text: "Studiare la teoria",
-                done: false
-            },
-    
-                {text: "Applicare la teoria alla pratica",
-                done: true
-            },
-    
-                {text: "Non funziona nulla",
-                done: true
-            },
-    
-                {text: "Piangere sul divano del napoli",
-                done: false
-            }],
-            newToDo  : "",
+            toDoList: [
+                {
+                    text : "Capire qualcosa di programmazione",
+                    done : true,
+                },
+                {
+                    text : "Provare a studiare la teoria",
+                    done : false,
+                },
+                {
+                    text : "Applicare la teoria alla pratica",
+                    done : false,
+                },
+                {
+                    text : "Non funziona nulla",
+                    done : true,
+                },
+                {
+                    text : "Piangere sul divano del Napoli",
+                    done : false,
+                },
+            ],
+            newListElement : "",
         }
     },
-
-    methods: {
-        removeToDo (whichToDo) {
-            this.ToDoList.splice (whichToDo, 1)
+    methods:{
+        removeElement(elementIndex){
+            this.toDoList.splice(elementIndex, 1);
         },
 
-        textLiner (item){
-            item.done = !item.done;
-        },
-
-        addNewToDo (newToDo) {
-            if (this.newToDo.text !== ""){
-                this.ToDoList.push ({text: newToDo, done: false })
+        addNewListElement(){
+            if (this.newListElement !== ""){
+                this.toDoList.push({text:this.newListElement, done:false});
+                this.newListElement= "";
             }
-            this.newToDo = ""
-        }
-    }
-}).mount ("#app")
+        },
+
+        textLiner(element){
+            element.done = !element.done;
+        },
+    },
 
 
 
-
+}).mount("#app");
